b9f6fd42bad4993e0242174cf67f4a495df06092
Jasonpro-maker <jason890312@gmail.com>
2026-07-19 20:51:04 +0800

    Fix iPhone home screen icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "daily-drink-recorder-v15";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
+const CACHE_NAME = "daily-drink-recorder-v16";
+const APP_SHELL = ["./", "./manifest.webmanifest", "./icon.svg", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
